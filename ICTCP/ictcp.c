e52a4ea5bc10da7de09e6985180e11fedfdb5277
wei bai <baiwei0427@sina.com>
2014-04-03 04:13:12 +0800

    Correct an TCP checksum error

--- ICTCP/ictcp.c
+++ ICTCP/ictcp.c
@@ -36,44 +36,50 @@ static struct nf_hook_ops nfho_outgoing;
 unsigned int hook_func_out(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *))
 {
 	struct iphdr *ip_header;         //IP  header struct
-    struct tcphdr *tcp_header;       //TCP header struct
-	unsigned short int src_port;     //TCP source port
+	struct tcphdr *tcp_header;       //TCP header struct
+	unsigned short int dst_port;     //TCP source port
 	int tcplen=0;                    //Length of TCP
 	
 	ip_header=(struct iphdr *)skb_network_header(skb);
 
 	//The packet is not ip packet (e.g. ARP or others)
-    if (!ip_header)
-    {
+	if (!ip_header)
+	{
 		return NF_ACCEPT;
-    }
+	}
 
 	if(ip_header->protocol==IPPROTO_TCP) //TCP
 	{
-		 tcp_header = (struct tcphdr *)((__u32 *)ip_header+ ip_header->ihl);
-         //Get source and destination TCP port
-         src_port=htons((unsigned short int) tcp_header->source);
-		 //We only deal with iperf traffic whose server port is 5001
-		 if(src_port==5001&&tcp_header->ack)
-		 {
+		tcp_header = (struct tcphdr *)((__u32 *)ip_header+ ip_header->ihl);
+		//Get source and destination TCP port
+		dst_port=htons((unsigned short int) tcp_header->dest);
+		//We only deal with iperf traffic whose server port is 5001
+		if(tcp_header->ack)//dst_port==5001&&//||tcp_header->syn))
+		{
 			if (skb_linearize(skb)!= 0) 
 			{
 				return NF_ACCEPT;
 			}
 			ip_header=(struct iphdr *)skb_network_header(skb);
 			tcp_header = (struct tcphdr *)((__u32 *)ip_header+ ip_header->ihl);
-			//1MSS=12*128
-			tcp_header->window=htons(1500);
+			//2MSS
+			tcp_header->window=htons(1460);
 
-			//TCP length=IP total length - IP header length
-			tcplen=ntohs(ip_header->tot_len)-(ip_header->ihl<<2);
+			//TCP length=Total length - IP header length
+			tcplen=skb->len-(ip_header->ihl<<2);
 			tcp_header->check=0;
-
+			
+			tcp_header->check = csum_tcpudp_magic(ip_header->saddr, ip_header->daddr,
+                                  tcplen, ip_header->protocol,
+                                  csum_partial((char *)tcp_header, tcplen, 0));
+								  
 			//static inline __sum16 tcp_v4_check(int len, __be32 saddr,__be32 daddr, __wsum base)
-			tcp_header->check = tcp_v4_check(tcplen,
-											 ip_header->saddr,
-											 ip_header->daddr,
-											 csum_partial((char *)tcp_header,tcplen,0));
+			//tcp_header->check = tcp_v4_check(tcplen,
+			//								 ip_header->saddr,
+			//								 ip_header->daddr,
+			//								 csum_partial((char *)tcp_header,tcplen,0));
+											 
+			skb->ip_summed = CHECKSUM_UNNECESSARY;
 			
 		 }
 	}
